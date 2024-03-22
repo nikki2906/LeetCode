a50229cac7d461ec4c22e827428da08b15910bff
nikki2906 <104235980+nikki2906@users.noreply.github.com>
2024-03-22 00:40:03 -0400

    :ballot_box_with_check: binary-search [Runtime: 189 ms; Memory: 12.7 MB]

---
+++ array/binary-search.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def search(self, nums, target):
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+            else: 
+                return mid
+        return -1
