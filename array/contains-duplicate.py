9f83e439f5f4956a74add2133a33a7dc5f3bf016
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-03-25 20:34:22 -0400

    :ballot_box_with_check: contains-duplicate [Runtime: 415 ms; Memory: 27.5 MB]

---
+++ array/contains-duplicate.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        hashSet =set()
+        for num in nums:
+            if num not in hashSet:
+                hashSet.add(num)
+            else:
+                return True
+        return False
