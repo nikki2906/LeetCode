17c6c347fbfaa6a18df3ba4d10d904a3a91f9469
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-03-26 01:42:57 -0400

    :ballot_box_with_check: balanced-binary-tree [Runtime: 26 ms; Memory: 17 MB]

--- binary-tree/balanced-binary-tree.py
+++ binary-tree/balanced-binary-tree.py
@@ -7,11 +7,12 @@
 class Solution(object):
     def isBalanced(self, root):
         def dfs(root):
-            if not root: 
+            if not root:
                 return [True, 0]
-            left = dfs(root.left) # return [T/F,  0]
+            left = dfs(root.left)
             right = dfs(root.right)
             balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
-            height = 1 + max(left[1], right[1])
+            height = max(left[1], right[1]) + 1
             return [balanced, height]
+
         return dfs(root)[0]
