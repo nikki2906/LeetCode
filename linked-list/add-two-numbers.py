eb4f54c729aa3e0b81c905d9b402d1d63d728db6
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-03-25 23:08:28 -0400

    :ballot_box_with_check: add-two-numbers [Runtime: 43 ms; Memory: 11.7 MB]

---
+++ linked-list/add-two-numbers.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        dummyNode = ListNode()
+        curr = dummyNode
+        carry = 0
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            val = val1 + val2 + carry
+            carry = val // 10
+            val = val % 10
+            curr.next = ListNode(val)
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            curr = curr.next
+        return dummyNode.next
