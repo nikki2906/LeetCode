5c3007ac922f7ee02be76c3a1f5a52599ee50aef
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-03-25 20:42:28 -0400

    :ballot_box_with_check: two-sum [Runtime: 35 ms; Memory: 12.5 MB]

---
+++ hash-table/two-sum.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        hashMap = {}
+        for i, num in enumerate(nums):
+            difference = target - num
+            if difference not in hashMap:
+                hashMap[num]= i
+            elif difference in hashMap:
+                return (hashMap[difference], i)
