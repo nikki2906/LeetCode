99cf7c91bcd8f575028aeabfd5cfa18c87e75550
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-04-03 18:06:29 -0400

    :ballot_box_with_check: best-time-to-buy-and-sell-stock [Runtime: 782 ms; Memory: 21 MB]

--- array/best-time-to-buy-and-sell-stock.py
+++ array/best-time-to-buy-and-sell-stock.py
@@ -2,12 +2,12 @@ class Solution(object):
     def maxProfit(self, prices):
         left = 0
         right = 1
-        max_curr = 0
+        curr_max = 0
         while right < len(prices):
             if prices[left] < prices[right]:
                 profit = prices[right] - prices[left]
-                max_curr = max(max_curr, profit)
+                curr_max = max(curr_max, profit)
             else:
                 left = right
             right += 1
-        return max_curr
+        return curr_max
