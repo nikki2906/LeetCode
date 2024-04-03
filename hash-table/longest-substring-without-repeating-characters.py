5343f182939f4ae402ab1799795104d4c473a409
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-04-03 19:19:55 -0400

    :ballot_box_with_check: longest-substring-without-repeating-characters [Runtime: 30 ms; Memory: 12.6 MB]

---
+++ hash-table/longest-substring-without-repeating-characters.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        left = 0
+        res = 0
+        charSet = set()
+        for right in range(len(s)):
+            while s[right] in charSet:
+                charSet.remove(s[left])
+                left += 1
+            charSet.add(s[right])
+            res = max(res, right - left + 1)
+        return res
