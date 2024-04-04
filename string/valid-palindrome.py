6b45f75db9854025d7f7dcf7d891f7be81965b0b
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-04-04 16:56:01 -0400

    :ballot_box_with_check: valid-palindrome [Runtime: 44 ms; Memory: 12.3 MB]

---
+++ string/valid-palindrome.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            while left < right and not self.isAlphanumeric(s[left]):
+                left += 1
+            while left < right and not self.isAlphanumeric(s[right]):
+                right -= 1
+            while s[left].lower() != s[right].lower():
+                return False
+            left = left + 1
+            right = right - 1
+        return True
+            
+        
+    def isAlphanumeric(self, c):
+        return (ord("A") <= ord(c) <= ord("Z") or 
+                ord("a") <= ord(c) <= ord("z") or 
+                ord("0") <= ord(c) <= ord("9"))
