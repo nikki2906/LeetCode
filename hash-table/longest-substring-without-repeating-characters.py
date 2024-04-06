457a3ae0a631cc7058622d800e24525c2e8dafbc
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-04-06 12:22:26 -0400

    :ballot_box_with_check: longest-substring-without-repeating-characters [Runtime: 46 ms; Memory: 12.5 MB]

--- hash-table/longest-substring-without-repeating-characters.py
+++ hash-table/longest-substring-without-repeating-characters.py
@@ -2,12 +2,13 @@ class Solution(object):
     def lengthOfLongestSubstring(self, s):
         left = 0
         charSet = set()
-        curr_max = 0
+        length = 0
         for right in range(len(s)):
             while s[right] in charSet:
                 charSet.remove(s[left])
                 left += 1
             while s[right] not in charSet:
                 charSet.add(s[right])
-                curr_max = max(curr_max, right - left + 1) # right - left + 1: gives the current window size
-        return curr_max
+                length = max(length, right - left + 1)
+        return length 
+        
