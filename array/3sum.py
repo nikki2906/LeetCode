d6b8c626f051e3af194165fa9f8514e19eeb2823
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-04-06 22:37:30 -0400

    :ballot_box_with_check: 3sum [Runtime: 736 ms; Memory: 15.5 MB]

--- array/3sum.py
+++ array/3sum.py
@@ -3,21 +3,20 @@ class Solution(object):
         # set up
         res = []
         nums.sort()
-        # first num
+        # first num 
         for i, a in enumerate(nums):
-            if i>0 and nums[i] == nums[i-1] :
+            if nums[i] == nums[i-1] and i>0:
                 continue
-            # set up ptrs
-            left = i + 1
+            # set up 2 ptrs
+            left = i+1
             right = len(nums) - 1
             while left < right:
                 three_sum = a + nums[left] + nums[right]
-                # two sum
                 if three_sum > 0:
                     right -= 1
                 elif three_sum < 0:
                     left += 1
-                else:
+                else: 
                     res.append([a, nums[left], nums[right]])
                     left += 1
                     while nums[left] == nums[left - 1] and left < right:
