cd9202f50ef25685033a0bae7139287282681200
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-04-07 13:24:01 -0400

    :ballot_box_with_check: two-sum-ii-input-array-is-sorted [Runtime: 83 ms; Memory: 12.3 MB]

--- binary-search/two-sum-ii-input-array-is-sorted.py
+++ binary-search/two-sum-ii-input-array-is-sorted.py
@@ -3,10 +3,10 @@ class Solution(object):
         left = 0
         right = len(numbers) - 1
         while left < right:
-            currSum = numbers[left] + numbers[right]
-            if currSum < target:
+            sum = numbers[left] + numbers[right]
+            if sum < target:
                 left += 1
-            elif currSum > target:
+            elif sum > target:
                 right -= 1
             else:
                 return [left + 1, right + 1]
