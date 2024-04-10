bcd60674b53118c8434e538efb9520034a0f51c9
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-04-10 19:37:09 -0400

    :ballot_box_with_check: longest-repeating-character-replacement [Runtime: 54 ms; Memory: 12.2 MB]

--- hash-table/longest-repeating-character-replacement.py
+++ hash-table/longest-repeating-character-replacement.py
@@ -5,8 +5,9 @@ class Solution(object):
         max_freq = 0
         for right in range(len(s)):
             count[s[right]] = 1 + count.get(s[right], 0)
-            max_freq = max(max_freq, count[s[right]])
+            max_freq = max(count[s[right]], max_freq)
             while (right - left + 1) - max_freq > k:
                 count[s[left]] -= 1
                 left += 1
+
         return (right - left + 1)
