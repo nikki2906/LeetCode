3062510d2d55e9f0d60b0671aa7170f081af28f4
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-04-10 19:09:56 -0400

    :ballot_box_with_check: best-time-to-buy-and-sell-stock [Runtime: 814 ms; Memory: 21 MB]

--- dynamic-programming/best-time-to-buy-and-sell-stock.py
+++ dynamic-programming/best-time-to-buy-and-sell-stock.py
@@ -1,15 +1,14 @@
 class Solution(object):
     def maxProfit(self, prices):
         left = 0
-        right = 1 
+        right = 1
         max_profit = 0
         while right < len(prices):
             if prices[left] > prices[right]:
                 left = right
-            elif prices[left] < prices[right]:
+            elif prices[right] > prices[left]:
                 profit = prices[right] - prices[left]
-                max_profit = max(max_profit, profit)
-
+                max_profit = max(profit, max_profit)
             right += 1
-
         return max_profit
+        
