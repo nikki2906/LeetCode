f4b0d1576790673d45304593b0d8909b1bbdcf3b
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-04-17 00:55:30 -0400

    :ballot_box_with_check: 3sum [Runtime: 755 ms; Memory: 15.3 MB]

--- two-pointers/3sum.py
+++ two-pointers/3sum.py
@@ -1,25 +1,30 @@
 class Solution(object):
     def threeSum(self, nums):
-        # set up 
+        # set up
+        res = [] # a list
         nums.sort()
-        res = []
-        # first number: check if the previous number = the current number
+
+        # first num
         for i, a in enumerate(nums):
-            if nums[i] == nums[i-1] and i > 0:
+            if i > 0 and nums[i-1] == nums[i]:
                 continue
-            # second number
+
+            # second num
             left = i + 1
             right = len(nums) - 1
             while left < right:
-                three_sum = a + nums[right] + nums[left]
+                three_sum = a + nums[left] + nums[right]
                 if three_sum > 0:
                     right -= 1
                 elif three_sum < 0:
                     left += 1
                 else:
-                    res.append([a, nums[left], nums[right]])
+                    res.append([a, nums[right], nums[left]])
                     left += 1
-                    while nums[left] == nums[left-1] and left < right:
+                    
+                    # shift the pointer
+                    while nums[left - 1] == nums[left] and left < right:
                         left += 1
+                    while nums[right - 1] == nums[right] and left < right:
+                        right -= 1
         return res
-        
