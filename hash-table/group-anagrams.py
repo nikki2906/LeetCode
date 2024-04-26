aecf5df6a6b2c2616fd2cbda53795f7bd31c1642
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-04-25 20:24:22 -0400

    :ballot_box_with_check: group-anagrams [Runtime: 67 ms; Memory: 16 MB]

--- hash-table/group-anagrams.py
+++ hash-table/group-anagrams.py
@@ -2,10 +2,9 @@ class Solution(object):
     def groupAnagrams(self, strs):
         hashMap = {}
         for word in strs:
-            char = tuple(sorted(word))
-            if char not in hashMap:
-                hashMap[char] = [word]
+            characters = tuple(sorted(word))
+            if characters not in hashMap:
+                hashMap[characters] = [word]
             else:
-                hashMap[char].append(word)
+                hashMap[characters].append(word)
         return list(hashMap.values())
-        
