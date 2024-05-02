54417849fa0b46d10e56c83ce33698263b7a3aec
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-02 13:56:45 -0400

    :ballot_box_with_check: group-anagrams [Runtime: 65 ms; Memory: 16.1 MB]

--- array/group-anagrams.py
+++ array/group-anagrams.py
@@ -4,7 +4,7 @@ class Solution(object):
         for word in strs:
             characters = tuple(sorted(word))
             if characters not in hashMap:
-                hashMap[characters] =[word]
+                hashMap[characters] = [word]
             else:
                 hashMap[characters].append(word)
         return list(hashMap.values())
