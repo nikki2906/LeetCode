bc2602f4c0f2d5529d14aade2d44e2e985cad54b
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-03 14:15:15 -0400

    :ballot_box_with_check: two-sum-ii-input-array-is-sorted [Runtime: 91 ms; Memory: 12.6 MB]

--- two-pointers/two-sum-ii-input-array-is-sorted.py
+++ two-pointers/two-sum-ii-input-array-is-sorted.py
@@ -3,11 +3,11 @@ class Solution(object):
         left = 0
         right = len(numbers) - 1
         while left < right:
-            two_sum = numbers[right] + numbers[left]
-            if two_sum > target:
-                right -= 1
-            elif two_sum < target:
+            two_sum = numbers[left] + numbers[right]
+            if two_sum < target:
                 left += 1
+            elif two_sum > target:
+                right -= 1
             else:
                 return [left + 1, right + 1]
         return []
