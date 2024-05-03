fa5a0ccf0c9223470527d6703bacaa171ccec357
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-02 22:12:41 -0400

    :ballot_box_with_check: container-with-most-water [Runtime: 548 ms; Memory: 22.4 MB]

--- two-pointers/container-with-most-water.py
+++ two-pointers/container-with-most-water.py
@@ -1,16 +1,15 @@
 class Solution(object):
     def maxArea(self, height):
-        max_area = 0
+        maxArea= 0
         left = 0
-        right = len(height) - 1 
+        right = len(height) - 1
         while left < right:
-            area = min(height[right], height[left]) * (right - left)
-            max_area = max(area, max_area)
-            if height[left] > height[right]:
-                right -= 1
-            elif height[right] > height[left]:
+            area = (right - left) * min(height[left], height[right])
+            maxArea = max(area, maxArea)
+            if height[left] < height[right]:
                 left += 1
-            else:
+            elif height[left] > height[right]:
                 right -= 1
-        return max_area
-        
+            else: 
+                right -= 1
+        return maxArea
