f742aaef82a4d523c5ab3133ec1db5fa16b941c0
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-05 02:27:10 -0400

    :ballot_box_with_check: valid-parentheses [Runtime: 14 ms; Memory: 11.8 MB]

--- stack/valid-parentheses.py
+++ stack/valid-parentheses.py
@@ -1,13 +1,13 @@
 class Solution(object):
     def isValid(self, s):
         stack = []
-        hashMap = {"}" : "{", ")" : "(", "]" : "["}
+        hashMap = {')':'(', '}':'{', ']':'['}
         for c in s:
             if c not in hashMap:
                 stack.append(c)
             elif stack and stack[-1] == hashMap[c]:
                 stack.pop()
             else:
-                return False 
+                return False
         return True if not stack else False
         
