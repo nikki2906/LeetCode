35841b7507b290ae196c1c21bc8de10a46a9caea
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-04 21:39:34 -0400

    :ballot_box_with_check: valid-palindrome [Runtime: 46 ms; Memory: 12.4 MB]

--- two-pointers/valid-palindrome.py
+++ two-pointers/valid-palindrome.py
@@ -5,15 +5,16 @@ class Solution(object):
         while left < right:
             while left < right and not self.isAlphanumeric(s[left]):
                 left += 1
-            while left < right and not self.isAlphanumeric(s[right]):
-                right -= 1 
-            if s[right].lower() != s[left].lower():
+            while left < right and not self. isAlphanumeric(s[right]):
+                right-= 1
+            if s[left].lower() != s[right].lower():
                 return False
             right -= 1
             left += 1
         return True
-
+    
     def isAlphanumeric(self, c):
-        return (ord("A") <= ord(c) <= ord("Z") or
-                ord("a") <= ord(c) <= ord("z") or 
-                ord("0") <= ord(c) <= ord("9"))
+        return (ord('A') <= ord(c) <= ord('Z') or
+                ord('a') <= ord(c) <= ord('z') or 
+                ord ('0') <= ord(c) <= ord('9'))
+        
