f74a68ac6ca96dc16aa321f795693af3c2e04330
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-06 02:53:32 -0400

    :ballot_box_with_check: daily-temperatures [Runtime: 1033 ms; Memory: 26.2 MB]

---
+++ monotonic-stack/daily-temperatures.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        stack = []
+        res = [0] * len(temperatures)
+        for index, temp in enumerate(temperatures):
+            while stack and temp > stack[-1][0]:
+                stackTemp, stackIndex = stack.pop()
+                res[stackIndex] = index - stackIndex
+            stack.append((temp, index))
+        return res
+        
