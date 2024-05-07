14d1ffbcda86fe308daaaebf9d16db2c2b1fd27d
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-07 19:33:55 -0400

    :ballot_box_with_check: evaluate-reverse-polish-notation [Runtime: 42 ms; Memory: 13.8 MB]

--- math/evaluate-reverse-polish-notation.py
+++ math/evaluate-reverse-polish-notation.py
@@ -7,12 +7,13 @@ class Solution(object):
             elif c == "*":
                 stack.append(stack.pop() * stack.pop())
             elif c == "-":
-                a, b = stack.pop(), stack.pop()
+                a = stack.pop()
+                b = stack.pop()
                 stack.append(b - a)
             elif c == "/":
-                a, b = stack.pop(), stack.pop()
+                a = stack.pop()
+                b = stack.pop()
                 stack.append(int(float(b) / a))
             else:
                 stack.append(int(c))
         return stack[0]
-        
