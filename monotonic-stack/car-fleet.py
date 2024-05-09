41a94a45f0c20eb7285b2c48b2ba2bb25a061040
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-08 21:35:18 -0400

    :ballot_box_with_check: car-fleet [Runtime: 678 ms; Memory: 39 MB]

--- monotonic-stack/car-fleet.py
+++ monotonic-stack/car-fleet.py
@@ -1,10 +1,10 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        pair = [(p, s) for p, s in zip(position, speed)]
-        pair.sort(reverse=True)
+        pairs = [(p, s) for p, s in zip(position, speed)]
+        pairs.sort(reverse = True)
         stack = []
-        for p, s in pair:  # Reverse Sorted Order
+        for p, s in pairs:
             stack.append((target - p) / s)
-            if len(stack) >= 2 and stack[-1] <= stack[-2]:
-                stack.pop()
+            while len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()     
         return len(stack)
