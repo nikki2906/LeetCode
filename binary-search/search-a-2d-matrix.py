5b5933cbd2c85b9bf95ec104bbeac4ddec740f6e
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-09 00:51:17 -0400

    :ballot_box_with_check: search-a-2d-matrix [Runtime: 19 ms; Memory: 12.1 MB]

--- binary-search/search-a-2d-matrix.py
+++ binary-search/search-a-2d-matrix.py
@@ -1,33 +1,32 @@
 class Solution(object):
     def searchMatrix(self, matrix, target):
-        # get the matrix dimension
         rows = len(matrix)
         cols = len(matrix[0])
-
-        # find the midRow
+        
         top = 0
         bottom = rows - 1
         while top <= bottom:
             midRow = (top + bottom) // 2
             if matrix[midRow][0] > target:
-                bottom = midRow - 1
+                bottom = midRow - 1    
             elif matrix[midRow][-1] < target:
                 top = midRow + 1
             else:
                 break
+        
         if not(top <= bottom):
-            return False
+            return 
 
-        # find the midIndex
-        left = 0
+        left = 0 
         right = cols - 1
         midRow = (top + bottom) // 2
         while left <= right:
-            midIndex = (left + right) // 2
-            if matrix[midRow][midIndex] > target:
-                right = midIndex - 1
-            elif matrix[midRow][midIndex] < target:
-                left = midIndex + 1
+            midPoint = (left + right) // 2
+            if matrix[midRow][midPoint] > target:
+                right = midPoint - 1
+            elif matrix[midRow][midPoint] < target:
+                left = midPoint + 1
             else:
                 return True
         return False
+            
