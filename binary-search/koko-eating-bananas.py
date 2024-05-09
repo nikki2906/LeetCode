3f448db2776b8f4b68aeef1969be0a063b1d9959
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-09 01:53:56 -0400

    :ballot_box_with_check: koko-eating-bananas [Runtime: 478 ms; Memory: 12.7 MB]

--- binary-search/koko-eating-bananas.py
+++ binary-search/koko-eating-bananas.py
@@ -1,16 +1,17 @@
 class Solution(object):
     def minEatingSpeed(self, piles, h):
-        left = 1 
+        left = 1
         right = max(piles)
         res = right
         while left <= right:
             k = (left + right) // 2
             total = 0
-            for pile in piles:
+            for pile in piles: 
                 total += math.ceil(float(pile) / k)
             if total <= h:
                 res = k
-                right = k - 1
+                right = k - 1 
             else:
                 left = k + 1
         return res
+        
