211c1f946fa6083209cdedd7c7e57f7aa18c66d7
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-10 23:16:48 -0400

    :ballot_box_with_check: koko-eating-bananas [Runtime: 474 ms; Memory: 13 MB]

--- binary-search/koko-eating-bananas.py
+++ binary-search/koko-eating-bananas.py
@@ -5,13 +5,12 @@ class Solution(object):
         res = right
         while left <= right:
             k = (left + right) // 2
-            total = 0
+            total = 0 
             for pile in piles:
                 total += math.ceil(float(pile) / k)
             if total <= h:
                 res = k
-                right = k - 1                
+                right = k - 1
             else:
                 left = k + 1
         return res
-        
