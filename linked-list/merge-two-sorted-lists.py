bfb07ba6191fa239036f3079bd2d977599d28f21
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-12 18:29:11 -0400

    :ballot_box_with_check: merge-two-sorted-lists [Runtime: 22 ms; Memory: 11.9 MB]

--- linked-list/merge-two-sorted-lists.py
+++ linked-list/merge-two-sorted-lists.py
@@ -5,21 +5,19 @@
 #         self.next = next
 class Solution(object):
     def mergeTwoLists(self, list1, list2):
-        # we return a new linked list to we create a new node
         dummyNode = ListNode()
         tail = dummyNode
-
         while list1 and list2:
             if list1.val < list2.val:
-                tail.next = list1 
+                tail.next = list1
                 list1 = list1.next
             else:
                 tail.next = list2
                 list2 = list2.next
             tail = tail.next
-        # remaining nodes
         if list1:
             tail.next = list1
         if list2:
             tail.next = list2
         return dummyNode.next
+        
