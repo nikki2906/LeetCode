d761a8a5e57c0bbce8d3dc1be9d25ebee13f44ea
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-14 15:19:37 -0400

    :ballot_box_with_check: longest-repeating-character-replacement [Runtime: 48 ms; Memory: 12.1 MB]

--- sliding-window/longest-repeating-character-replacement.py
+++ sliding-window/longest-repeating-character-replacement.py
@@ -2,11 +2,11 @@ class Solution(object):
     def characterReplacement(self, s, k):
         left = 0
         count = {}
-        mostFreq = 0
+        maxFreq  = 0
         for right in range(len(s)):
-            count[s[right]] =  1 + count.get(s[right], 0)
-            mostFreq = max(mostFreq, count[s[right]])
-            while (right - left + 1) - mostFreq > k:
+            count[s[right]] = 1 + count.get(s[right], 0)
+            maxFreq = max(maxFreq, count[s[right]])
+            while (right - left + 1) - maxFreq > k:
                 count[s[left]] -= 1
                 left += 1
-        return (right - left + 1)        
+        return (right - left + 1)
