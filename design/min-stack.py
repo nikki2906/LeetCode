294589bd0d759462aed77241aa454eb41ee31222
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-14 16:12:44 -0400

    :ballot_box_with_check: min-stack [Runtime: 34 ms; Memory: 15.5 MB]

--- design/min-stack.py
+++ design/min-stack.py
@@ -1,23 +1,27 @@
-class MinStack:
+class MinStack(object):
 
     def __init__(self):
         self.stack = []
         self.minStack = []
 
-    def push(self, val: int) -> None:
+
+    def push(self, val):
         self.stack.append(val)
         val = min(val, self.minStack[-1] if self.minStack else val)
         self.minStack.append(val)
 
-    def pop(self) -> None:
+    def pop(self):
         self.stack.pop()
         self.minStack.pop()
+        
 
-    def top(self) -> int:
+    def top(self):
         return self.stack[-1]
+        
 
-    def getMin(self) -> int:
+    def getMin(self):
         return self.minStack[-1]
+        
 
 
 # Your MinStack object will be instantiated and called as such:
