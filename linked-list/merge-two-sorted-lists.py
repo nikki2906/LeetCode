230eecd02cdd3c2fa4cff91264db9887e50989f5
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-19 19:57:57 -0400

    :ballot_box_with_check: merge-two-sorted-lists [Runtime: 38 ms; Memory: 16.6 MB]

--- linked-list/merge-two-sorted-lists.py
+++ linked-list/merge-two-sorted-lists.py
@@ -15,7 +15,6 @@ class Solution:
                 tail.next = list2
                 list2 = list2.next
             tail = tail.next
-        
         if list1:
             tail.next = list1
         if list2:
