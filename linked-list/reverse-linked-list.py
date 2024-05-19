c8d6ac1dd5015b2cb13924a2507b8c552c78a629
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-19 19:55:39 -0400

    :ballot_box_with_check: reverse-linked-list [Runtime: 36 ms; Memory: 17.7 MB]

--- linked-list/reverse-linked-list.py
+++ linked-list/reverse-linked-list.py
@@ -5,8 +5,8 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prev = None
         curr = head
+        prev = None
         while curr:
             temp = curr.next
             curr.next = prev
