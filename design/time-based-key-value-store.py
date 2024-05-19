b39e213e02d8503874d72fadd836e4644ee4f2a2
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-18 21:01:24 -0400

    :ballot_box_with_check: time-based-key-value-store [Runtime: 570 ms; Memory: 74.7 MB]

---
+++ design/time-based-key-value-store.py
@@ -0,0 +1,31 @@
+class TimeMap:
+
+    def __init__(self):
+        self.keyStore = {}
+        # keyStore: list of [value, time stamp]
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.keyStore:
+            self.keyStore[key] = []
+        self.keyStore[key].append([value, timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+        res = "" # empty string
+        values = self.keyStore.get(key, [])
+        left = 0
+        right = len(values) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if values[mid][1] <= timestamp:
+                res = values[mid][0]
+                left = mid + 1
+            else:
+                right= mid - 1
+        return res
+
+
+
+# Your TimeMap object will be instantiated and called as such:
+# obj = TimeMap()
+# obj.set(key,value,timestamp)
+# param_2 = obj.get(key,timestamp)
