bb2a02615c5f1c77f200cb2a6e4ba511088add64
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-21 22:19:25 -0400

    :ballot_box_with_check: diameter-of-binary-tree [Runtime: 40 ms; Memory: 19.1 MB]

---
+++ tree/diameter-of-binary-tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        res = [0]
+
+        def dfs(root):
+            if not root:
+                return -1
+            # get the heights
+            left = dfs(root.left)
+            right = dfs(root.right)
+            # get the diameter
+            res[0] = max(res[0], 2 + left + right)
+
+            # return the height
+            return 1 + max(left, right)
+        dfs(root)
+        return res[0]
+        
