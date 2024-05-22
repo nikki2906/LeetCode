72e11e848b5e4514dcb1eb4bf1626c3859388c48
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-21 22:45:43 -0400

    :ballot_box_with_check: balanced-binary-tree [Runtime: 31 ms; Memory: 17.4 MB]

--- tree/balanced-binary-tree.py
+++ tree/balanced-binary-tree.py
@@ -11,9 +11,7 @@ class Solution(object):
                 return [True, 0]
             left = dfs(root.left)
             right = dfs(root.right)
-            balanced = left[0] and right[0] and abs(left[1] - right[1]) <= 1
-            height = max(left[1], right[1]) + 1
-            return (balanced, height)
+            balance = left[0] and right[0] and abs(left[1] - right[1]) <= 1 
+            height = 1 + max(left[1], right[1])
+            return [balance, height]
         return dfs(root)[0]
-
-        
