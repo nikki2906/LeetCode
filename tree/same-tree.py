559a4970bddbfbb38af9fa8d4eb337d0c019e5df
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-21 23:39:07 -0400

    :ballot_box_with_check: same-tree [Runtime: 8 ms; Memory: 11.6 MB]

--- tree/same-tree.py
+++ tree/same-tree.py
@@ -8,7 +8,9 @@ class Solution(object):
     def isSameTree(self, p, q):
         if not p and not q:
             return True
-        if p and q and p.val == q.val:
-            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-        else:
+        if not p or not q or p.val != q.val:
             return False
+        return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
+        
+
+        
