7442a0b2efb4967f391dc0e693e2079afb3bcf97
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-24 14:33:43 -0400

    :ballot_box_with_check: maximum-depth-of-binary-tree [Runtime: 19 ms; Memory: 14.3 MB]

--- binary-tree/maximum-depth-of-binary-tree.py
+++ binary-tree/maximum-depth-of-binary-tree.py
@@ -8,5 +8,7 @@ class Solution(object):
     def maxDepth(self, root):
         if not root:
             return 0
-        return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+        return 1 + max(self.maxDepth(root.right), self.maxDepth(root.left))
+
+            
         
