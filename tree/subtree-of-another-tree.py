c265ffc1de52c2224a53044c895d0016ba172115
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-28 22:05:08 -0400

    :ballot_box_with_check: subtree-of-another-tree [Runtime: 105 ms; Memory: 12.5 MB]

--- tree/subtree-of-another-tree.py
+++ tree/subtree-of-another-tree.py
@@ -5,18 +5,18 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
-    def isSubtree(self, s, t):
-        if not t:
+    def isSubtree(self, root, subRoot):
+        if not subRoot:
             return True
-        if not s:
+        if not root:
             return False
-        if self.sameTree(s, t):
+        if self.sameTree(root, subRoot):
             return True
-        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t) 
-    def sameTree(self, s, t):
-        if not s and not t:
+        return (self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot))
+     
+    def sameTree(self, root, subRoot):
+        if not root and not subRoot:
             return True
-        if s and t and s.val == t.val:
-            return self.sameTree(s.left, t.left) and self.sameTree(s.right, t.right)
-        else:
-            return False
+        if root and subRoot and root.val == subRoot.val:
+            return (self.sameTree(root.left, subRoot.left) and self.sameTree(root.right, subRoot.right))
+        return False
