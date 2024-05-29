4a3e982eae77310593c5e8c32dfa8b96a1641add
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-29 19:30:18 -0400

    :ballot_box_with_check: invert-binary-tree [Runtime: 35 ms; Memory: 16.5 MB]

--- binary-tree/invert-binary-tree.py
+++ binary-tree/invert-binary-tree.py
@@ -1,11 +1,11 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def invertTree(self, root):
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
             return None
 
@@ -15,5 +15,5 @@ class Solution(object):
 
         self.invertTree(root.left)
         self.invertTree(root.right)
+
         return root
-        
