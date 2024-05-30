5fe8a6dde100c6ab11d2dee1718f8aeab9ac3586
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-29 22:17:56 -0400

    :ballot_box_with_check: binary-tree-right-side-view [Runtime: 41 ms; Memory: 16.4 MB]

---
+++ depth-first-search/binary-tree-right-side-view.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        queue = collections.deque([root])
+        while queue:
+            rightSide = None
+            queueLen = len(queue)
+            for i in range(queueLen):
+                node = queue.popleft()
+                if node:
+                    rightSide = node
+                    queue.append(node.left)
+                    queue.append(node.right)
+            if rightSide:
+                res.append(rightSide.val)
+        return res
+
+        
