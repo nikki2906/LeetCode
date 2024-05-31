ae5f3a9e767a3d7ed94ab54fbe5f5ad2d86591dd
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-31 19:21:44 -0400

    :ballot_box_with_check: subtree-of-another-tree [Runtime: 94 ms; Memory: 12.6 MB]

--- string-matching/subtree-of-another-tree.py
+++ string-matching/subtree-of-another-tree.py
@@ -13,9 +13,11 @@ class Solution(object):
         if self.sameTree(root, subRoot):
             return True
         return (self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot))
+        
 
     def sameTree(self, root, subRoot):
         if not root and not subRoot:
             return True
         if root and subRoot and root.val == subRoot.val:
             return (self.sameTree(root.left, subRoot.left) and self.sameTree(root.right, subRoot.right))
+        
