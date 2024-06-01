87fbf722cc57e10e97900993be0a941b752f3376
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-05-31 23:38:37 -0400

    :ballot_box_with_check: validate-binary-search-tree [Runtime: 48 ms; Memory: 18.3 MB]

--- tree/validate-binary-search-tree.py
+++ tree/validate-binary-search-tree.py
@@ -9,8 +9,8 @@ class Solution:
         def valid(node, left, right):
             if not node:
                 return True
-            if not(left < node.val and right > node.val):
+            if not(node.val < right and node.val > left):
                 return False
             return (valid(node.left, left, node.val) and valid(node.right, node.val, right))
-
-        return valid(root, float('-inf'), float('inf'))
+        return valid(root, float('-inf'), float('+inf'))
+        
