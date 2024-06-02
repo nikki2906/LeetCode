f15fc7503c3928891cc4b91a2a0c29a812a2afc6
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-06-02 15:38:51 -0400

    :ballot_box_with_check: balanced-binary-tree [Runtime: 29 ms; Memory: 17.1 MB]

--- binary-tree/balanced-binary-tree.py
+++ binary-tree/balanced-binary-tree.py
@@ -1,22 +1,18 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+class Solution(object):
+    def isBalanced(self, root):
         def dfs(root):
             if not root:
                 return [True, 0]
             left = dfs(root.left)
             right = dfs(root.right)
-
-            balance = (left[0] and right[0] and abs(right[1] - left[1]) <=1)
+            balance = left[0] and right[0] and abs(left[1] - right[1]) <= 1
             height = 1 + max(left[1], right[1])
-
+            
             return [balance, height]
         return dfs(root)[0]
-
-            
-        
