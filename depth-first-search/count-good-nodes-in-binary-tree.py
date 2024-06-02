47ea0fdce643bfe0c3ba53c70ec2ba67bc417564
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-06-02 16:33:27 -0400

    :ballot_box_with_check: count-good-nodes-in-binary-tree [Runtime: 134 ms; Memory: 31.5 MB]

--- depth-first-search/count-good-nodes-in-binary-tree.py
+++ depth-first-search/count-good-nodes-in-binary-tree.py
@@ -9,8 +9,8 @@ class Solution:
         def dfs(node, maxVal):
             if not node:
                     return 0
-            res = 1 if node.val >= maxVal else 0
             maxVal = max(maxVal, node.val)
+            res = 1 if node.val >= maxVal else 0    
             res += dfs(node.left, maxVal)
             res += dfs(node.right, maxVal)
             return res
