ba9701f56e3d76c5e6f60e1e79d0650b5686fc1a
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-06-02 15:42:34 -0400

    :ballot_box_with_check: same-tree [Runtime: 15 ms; Memory: 11.7 MB]

--- breadth-first-search/same-tree.py
+++ breadth-first-search/same-tree.py
@@ -1,11 +1,11 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+class Solution(object):
+    def isSameTree(self, p, q):
         if not p and not q:
             return True
         if not p or not q:
@@ -13,4 +13,3 @@ class Solution:
         if p.val != q.val:
             return False
         return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
-        
