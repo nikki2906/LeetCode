adbbf0b3f00ed6068196a565bbdb4ae5abaa7216
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-06-02 15:30:11 -0400

    :ballot_box_with_check: maximum-depth-of-binary-tree [Runtime: 22 ms; Memory: 14.4 MB]

--- tree/maximum-depth-of-binary-tree.py
+++ tree/maximum-depth-of-binary-tree.py
@@ -1,11 +1,11 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def maxDepth(self, root: Optional[TreeNode]) -> int:
+class Solution(object):
+    def maxDepth(self, root):
         if not root:
             return 0
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
