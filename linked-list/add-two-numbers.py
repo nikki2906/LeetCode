37f3e1c45534ff6e91cc1775697a52cc2e512e6e
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-06-03 23:01:46 -0400

    :ballot_box_with_check: add-two-numbers [Runtime: 58 ms; Memory: 16.6 MB]

--- linked-list/add-two-numbers.py
+++ linked-list/add-two-numbers.py
@@ -6,17 +6,18 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         dummyNode = ListNode()
-        tail = dummyNode
+        curr = dummyNode
         carry = 0
         while l1 or l2 or carry:
             val1 = l1.val if l1 else 0
-            val2 = l2.val if l2 else 0 
-            total = val1 + val2 + carry
-            carry = total // 10
-            val = total % 10
-            tail.next = ListNode(val)
-            tail = tail.next
+            val2 = l2.val if l2 else 0
+            val = val1 + val2 + carry 
+            carry = val // 10
+            val = val % 10
+            curr.next = ListNode(val)
 
+            # shift the pointers and the list
+            curr = curr.next
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
         return dummyNode.next
