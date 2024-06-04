b52567979895a0e3c1db901888473abd99ca2a94
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-06-03 20:35:58 -0400

    :ballot_box_with_check: diameter-of-binary-tree [Runtime: 36 ms; Memory: 19.2 MB]

--- depth-first-search/diameter-of-binary-tree.py
+++ depth-first-search/diameter-of-binary-tree.py
@@ -1,11 +1,11 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def diameterOfBinaryTree(self, root):
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         res = [0]
         def dfs(root):
             if not root:
@@ -13,6 +13,7 @@ class Solution(object):
             left = dfs(root.left)
             right = dfs(root.right)
             res[0] = max(res[0], 2 + left + right)
-            return 1 + max(left, right)        
+            return 1 + max(left, right)
         dfs(root)
         return res[0]
+        
