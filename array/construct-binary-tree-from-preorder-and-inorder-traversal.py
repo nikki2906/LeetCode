3946516af114f7292b19f27542489434e900cabc
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-06-03 22:51:39 -0400

    :ballot_box_with_check: construct-binary-tree-from-preorder-and-inorder-traversal [Runtime: 121 ms; Memory: 88.1 MB]

--- array/construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ array/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -10,6 +10,6 @@ class Solution:
             return None
         root = TreeNode(preorder[0])
         mid = inorder.index(preorder[0])
-        root.left =  self.buildTree(preorder[1:mid + 1], inorder[:mid])
-        root.right = self.buildTree(preorder[mid + 1:], inorder[mid + 1:])
-        return root
+        root.left = self.buildTree(preorder[1:mid+1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid+1:], inorder[mid+1:])
+        return root  
