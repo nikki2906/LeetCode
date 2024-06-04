ea8469ccbe4d591dbb3971c9f59c8e698eb8d16d
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-06-04 19:51:37 -0400

    :ballot_box_with_check: add-two-numbers [Runtime: 53 ms; Memory: 16.6 MB]

--- recursion/add-two-numbers.py
+++ recursion/add-two-numbers.py
@@ -11,13 +11,13 @@ class Solution:
         while l1 or l2 or carry:
             val1 = l1.val if l1 else 0
             val2 = l2.val if l2 else 0
-            val = val1 + val2 + carry 
+            val = carry + val1 + val2
             carry = val // 10
             val = val % 10
-            curr.next = ListNode(val)
 
-            # shift the pointers and the list
+            curr.next = ListNode(val)
             curr = curr.next
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
         return dummyNode.next
+        
