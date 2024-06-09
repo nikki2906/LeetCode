00bd0700c836c1f344abd192154468e44ab98801
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-06-09 19:41:35 -0400

    :ballot_box_with_check: binary-tree-level-order-traversal [Runtime: 19 ms; Memory: 12.3 MB]

--- tree/binary-tree-level-order-traversal.py
+++ tree/binary-tree-level-order-traversal.py
@@ -1,11 +1,11 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+class Solution(object):
+    def levelOrder(self, root):
         res = []
         queue = collections.deque([root])
         while queue:
@@ -17,5 +17,5 @@ class Solution:
                     queue.append(node.left)
                     queue.append(node.right)
             if level:
-                res.append(level)       
-        return res 
+                res.append(level)
+        return res
