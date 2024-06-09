1fb84d78115fd81c57464568bf266094985272b7
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-06-09 19:13:34 -0400

    :ballot_box_with_check: lowest-common-ancestor-of-a-binary-search-tree [Runtime: 40 ms; Memory: 19.6 MB]

--- binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -8,10 +8,11 @@
 class Solution(object):
     def lowestCommonAncestor(self, root, p, q):
         curr = root
-        while curr:
+        while curr: 
             if p.val > curr.val and q.val > curr.val:
                 curr = curr.right
             elif p.val < curr.val and q.val < curr.val:
                 curr = curr.left
-            else: 
+            else:
                 return curr
+        
