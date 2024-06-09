9db84379d015b92c1a7b27e10cea4ed9c3d27fe2
Nhi Huynh <104235980+nikki2906@users.noreply.github.com>
2024-06-09 01:39:15 -0400

    :ballot_box_with_check: subtree-of-another-tree [Runtime: 98 ms; Memory: 12.8 MB]

--- string-matching/subtree-of-another-tree.py
+++ string-matching/subtree-of-another-tree.py
@@ -1,23 +1,22 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+class Solution(object):
+    def isSubtree(self, root, subRoot):
         if not subRoot:
             return True
         elif not root:
             return False
-        elif self.sameTree(root, subRoot):
+        elif self.isSameTree(root, subRoot):
             return True
         return (self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot))
-
-    def sameTree(self, root, subRoot):
+        
+    def isSameTree(self, root, subRoot):
         if not root and not subRoot:
             return True
-        if root and subRoot and root.val == subRoot.val:
-            return (self.sameTree(root.left, subRoot.left) and self.sameTree(root.right, subRoot.right))
+        elif root and subRoot and root.val == subRoot.val:
+            return (self.isSameTree(root.left, subRoot.left) and self.isSameTree(root.right, subRoot.right))
         return False
-        
