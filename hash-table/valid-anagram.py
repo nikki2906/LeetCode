4a1bb67d65cfb0cd618bf76bc513e35b8694fb44
nhi2906 <104235980+nhi2906@users.noreply.github.com>
2024-06-22 23:13:08 -0400

    :ballot_box_with_check: valid-anagram [Runtime: 32 ms; Memory: 14 MB]

--- hash-table/valid-anagram.py
+++ hash-table/valid-anagram.py
@@ -2,12 +2,14 @@ class Solution(object):
     def isAnagram(self, s, t):
         if len(s) != len(t):
             return False
-        countS={}
+        countS = {}
         for i in range(len(s)):
             countS[s[i]] = 1 + countS.get(s[i], 0)
-        countT={}
+
+        countT = {}
         for i in range(len(t)):
             countT[t[i]] = 1 + countT.get(t[i], 0)
+
         for c in countS:
             if countS[c] != countT.get(c, 0):
                 return False
