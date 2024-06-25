b73447cc8067dfc58d49a15b250a51ab6f11727f
nikki2906 <104235980+nikki2906@users.noreply.github.com>
2024-06-24 23:42:22 -0400

    :ballot_box_with_check: longest-consecutive-sequence [Runtime: 335 ms; Memory: 28.9 MB]

--- union-find/longest-consecutive-sequence.py
+++ union-find/longest-consecutive-sequence.py
@@ -1,11 +1,11 @@
 class Solution(object):
     def longestConsecutive(self, nums):
-        hashSet =set(nums)
+        hashSet = set(nums)
         longest = 0
         for num in hashSet:
             if (num - 1) not in hashSet:
                 length = 0
                 while (num + length) in hashSet:
                     length += 1
-                longest = max(length, longest)
+                longest = max(longest, length)
         return longest
