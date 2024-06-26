9a674aa25c87e4b2ec4fc4d477277037630d80db
nikki2906 <104235980+nikki2906@users.noreply.github.com>
2024-06-25 22:26:32 -0400

    :ballot_box_with_check: 3sum [Runtime: 735 ms; Memory: 15.3 MB]

--- array/3sum.py
+++ array/3sum.py
@@ -1,18 +1,14 @@
 class Solution(object):
     def threeSum(self, nums):
-        # set up
         res = []
         nums.sort()
-
-        # 1st integer
         for i, a in enumerate(nums):
-            if nums[i-1] == nums[i] and i>0:
+            if nums[i] == nums[i-1] and i>0:
                 continue
-            # 2nd and 3rd integers
-            left = i+1
+            left = i + 1
             right = len(nums) - 1
             while left < right:
-                threeSum = a + nums[left] + nums[right]
+                threeSum = nums[left] + nums[right] + a
                 if threeSum > 0:
                     right -= 1
                 elif threeSum < 0:
@@ -20,7 +16,7 @@ class Solution(object):
                 else:
                     res.append([a, nums[left], nums[right]])
                     left += 1
-                    while nums[left-1] == nums[left] and left < right:
+                    while nums[left] == nums[left-1] and left < right:
                         left += 1
         return res
-        
+    
