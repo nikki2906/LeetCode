27485c16d87ac0789ac12dabae3217578517281b
nikki2906 <104235980+nikki2906@users.noreply.github.com>
2024-06-27 23:28:57 -0400

    :ballot_box_with_check: product-of-array-except-self [Runtime: 279 ms; Memory: 19.1 MB]

--- array/product-of-array-except-self.py
+++ array/product-of-array-except-self.py
@@ -3,10 +3,10 @@ class Solution(object):
         res = [1] * len(nums)
         prefix = 1
         for i in range(len(nums)):
-            res[i] = prefix
+            res[i] = prefix 
             prefix *= nums[i]
         postfix = 1
         for i in range(len(nums) -1, -1, -1):
             res[i] *= postfix
             postfix *= nums[i]
-        return res        
+        return res
