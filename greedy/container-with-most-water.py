27075f4c2a887ba9c986376641f6c7ff50e9bd3c
nikki2906 <104235980+nikki2906@users.noreply.github.com>
2024-06-29 00:27:16 -0400

    :ballot_box_with_check: container-with-most-water [Runtime: 540 ms; Memory: 22.1 MB]

--- greedy/container-with-most-water.py
+++ greedy/container-with-most-water.py
@@ -3,14 +3,13 @@ class Solution(object):
         maxArea = 0
         left = 0
         right = len(height) - 1
-        while left <= right:
-            area = (right - left) * min(height[left], height[right])
+        while left < right:
+            area = min(height[left], height[right]) * (right - left)
             maxArea = max(area, maxArea)
-            if height[left] < height[right]:
-                left += 1
-            elif height[left] > height[right]:
+            if height[left] > height[right]:
                 right -= 1
+            elif height[left] < height[right]:
+                left += 1
             else:
                 right -= 1
         return maxArea
-        
