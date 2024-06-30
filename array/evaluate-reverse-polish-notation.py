dfc13d4b643738da3d24b04a60aa03e52362acc1
nikki2906 <104235980+nikki2906@users.noreply.github.com>
2024-06-30 17:26:16 -0400

    :ballot_box_with_check: evaluate-reverse-polish-notation [Runtime: 30 ms; Memory: 13.6 MB]

--- array/evaluate-reverse-polish-notation.py
+++ array/evaluate-reverse-polish-notation.py
@@ -2,19 +2,18 @@ class Solution(object):
     def evalRPN(self, tokens):
         stack = []
         for c in tokens:
-            if c == "+":
+            if c == '+':
                 stack.append(stack.pop() + stack.pop())
-            elif c == "*":
+            elif c == '*':
                 stack.append(stack.pop() * stack.pop())
-            elif c == "-":
+            elif c == '-':
                 a = stack.pop()
                 b = stack.pop()
                 stack.append(b - a)
-            elif c == "/":
+            elif c == '/':
                 a = stack.pop()
                 b = stack.pop()
                 stack.append(int(float(b) / a))
             else:
                 stack.append(int(c))
         return stack[0]
-        
