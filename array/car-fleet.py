e18e7661c37dc9ed87dcc9d3558df40f78acd576
nikki2906 <104235980+nikki2906@users.noreply.github.com>
2024-06-30 23:33:21 -0400

    :ballot_box_with_check: car-fleet [Runtime: 791 ms; Memory: 42.7 MB]

--- array/car-fleet.py
+++ array/car-fleet.py
@@ -1,11 +1,10 @@
 class Solution(object):
     def carFleet(self, target, position, speed):
-        pairs = [(p,s) for p, s in zip(position, speed)]
-        pairs.sort(reverse = True)
+        pair = [(p, s) for p, s in zip(position, speed)]
+        pair.sort(reverse = True)
         stack = []
-        for p, s in pairs:
-            stack.append((target - p) / s)
+        for p, s in pair:
+            stack.append(float(target - p) / s)
             if len(stack) >= 2 and stack[-1] <= stack[-2]:
                 stack.pop()
         return len(stack)
-            
