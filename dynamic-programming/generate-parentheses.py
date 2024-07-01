20a2e7ae2e9482dbdb6803ad76c5b7865209e76f
nikki2906 <104235980+nikki2906@users.noreply.github.com>
2024-07-01 00:24:17 -0400

    :ballot_box_with_check: generate-parentheses [Runtime: 16 ms; Memory: 11.8 MB]

--- dynamic-programming/generate-parentheses.py
+++ dynamic-programming/generate-parentheses.py
@@ -5,14 +5,14 @@ class Solution(object):
         def backtrack(openCount, closedCount):
             if openCount == closedCount == n:
                 res.append(''.join(stack))
-                return
-            if openCount < n:
-                stack.append('(')
-                backtrack(openCount + 1, closedCount)
-                stack.pop()
+                return 
             if openCount > closedCount:
                 stack.append(')')
                 backtrack(openCount, closedCount + 1)
+                stack.pop()
+            if openCount < n:
+                stack.append('(')
+                backtrack(openCount + 1, closedCount)
                 stack.pop()
         backtrack(0, 0)
         return res
