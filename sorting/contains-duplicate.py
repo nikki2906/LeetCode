0cf0e29e155f8d0d9ac82b1a641892d9bd1ac350
nikki2906 <104235980+nikki2906@users.noreply.github.com>
2024-08-27 23:14:03 -0400

    LC-contains-duplicate [Runtime: 415 ms; Memory: 31.8 MB]

--- sorting/contains-duplicate.py
+++ sorting/contains-duplicate.py
@@ -1,9 +1,9 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
-        hashMap = set()
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashSet = set()
         for num in nums:
-            if num not in hashMap:
-                hashMap.add(num)
+            if num not in hashSet:
+                hashSet.add(num)
             else:
                 return True
         return False
