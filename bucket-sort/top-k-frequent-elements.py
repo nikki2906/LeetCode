a417779138973a3072db0bfcf03143c9a2b0c498
nikki2906 <104235980+nikki2906@users.noreply.github.com>
2024-08-28 00:49:20 -0400

    LC-top-k-frequent-elements [Runtime: 79 ms; Memory: 21 MB]

--- bucket-sort/top-k-frequent-elements.py
+++ bucket-sort/top-k-frequent-elements.py
@@ -1,11 +1,11 @@
-class Solution(object):
-    def topKFrequent(self, nums, k):
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         hashMap = {}
         for num in nums:
             if num not in hashMap:
                 hashMap[num] = 1
             else:
                 hashMap[num] += 1
-        sortedNum = sorted(hashMap, key = hashMap.get, reverse = True)
-        return sortedNum[:k]
-            
+        sorted_nums = sorted(hashMap, key = hashMap.get, reverse = True)
+        return sorted_nums[:k]
+        
