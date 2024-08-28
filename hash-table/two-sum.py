24088a6554ec1d5b6cc6c597cf20c6df43cf052a
nikki2906 <104235980+nikki2906@users.noreply.github.com>
2024-08-27 23:38:42 -0400

    LC-two-sum [Runtime: 60 ms; Memory: 17.7 MB]

--- hash-table/two-sum.py
+++ hash-table/two-sum.py
@@ -1,5 +1,5 @@
-class Solution(object):
-    def twoSum(self, nums, target):
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashMap = {}
         for i, num in enumerate(nums):
             difference = target - num
