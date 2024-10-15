ebdf7ccce4d514cc44ad9675c9a9fec0a0db83a8
nikki2906 <104235980+nikki2906@users.noreply.github.com>
2024-10-15 14:17:25 -0400

    LC-best-time-to-buy-and-sell-stock [Runtime: 768 ms; Memory: 20.7 MB]

--- dynamic-programming/best-time-to-buy-and-sell-stock.py
+++ dynamic-programming/best-time-to-buy-and-sell-stock.py
@@ -4,10 +4,11 @@ class Solution(object):
         right = 1
         maxProfit = 0
         while right < len(prices):
-            if prices[left] > prices[right]:
-                left = right
-            elif prices[left] < prices[right]:
+            if prices[left] < prices[right]:
                 profit = prices[right] - prices[left]
                 maxProfit = max(profit, maxProfit)
+            elif prices[left] > prices[right]:
+                left = right
             right += 1
         return maxProfit
+        
