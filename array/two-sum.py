986c175932dd70ce11931f52243f5d6ee7829ae9
nikki2906 <104235980+nikki2906@users.noreply.github.com>
2024-10-23 11:47:52 -0400

    LC-two-sum [Runtime: 0 ms; Memory: 12.1 MB]

--- array/two-sum.py
+++ array/two-sum.py
@@ -1,9 +1,10 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+class Solution(object):
+    def twoSum(self, nums, target):
         hashMap = {}
         for i, num in enumerate(nums):
             difference = target - num
             if difference not in hashMap:
-                hashMap[num] = i 
+                hashMap[num] = i
             else:
                 return (hashMap[difference], i)
+        
