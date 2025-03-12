783476b9e074f63fb8047636898bd66578a61d80
nikki2906 <104235980+nikki2906@users.noreply.github.com>
2025-03-12 11:33:18 +0100

    LC-roman-to-integer [Runtime: 13 ms; Memory: 12.5 MB]

--- hash-table/roman-to-integer.py
+++ hash-table/roman-to-integer.py
@@ -1,11 +1,19 @@
 class Solution(object):
     def romanToInt(self, s):
-        roman = {"I":1, "V":5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
-        res = 0
+        hashMap = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        result = 0
         for i in range(len(s)):
-            if i + 1 < len(s) and roman[s[i]] < roman[s[i+1]]:
-                res -= roman[s[i]]
+            if i < len(s) - 1 and hashMap[s[i]] < hashMap[s[i+1]]:
+                result -= hashMap[s[i]]
             else:
-                res += roman[s[i]]
-        return res
-        
+                result += hashMap[s[i]]
+        return result
+                
