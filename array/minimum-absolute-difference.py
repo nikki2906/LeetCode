c556064ad8569a6b461ba95c6ad386f5cdc6b108
nikki2906 <104235980+nikki2906@users.noreply.github.com>
2026-03-19 02:28:03 -0400

    LC-minimum-absolute-difference [Runtime: N/A; Memory: N/A]

---
+++ array/minimum-absolute-difference.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        arr.sort()
+        minDiff = arr[1] - arr[0]
+
+        for i in range(1, len(arr)-1):
+            diff = arr[i+1] - arr[i]
+            if diff < minDiff:
+                minDiff == diff
+        
+        result = []
+        for i in range(len(arr)-1): 
+            if arr[i+1] - arr[i] == minDiff:
+                result.append([arr[i], arr[i+1]])
+        return result
