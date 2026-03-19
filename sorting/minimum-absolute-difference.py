4c829304f697bea49fb31ca3b7b62e757f7a4182
nikki2906 <104235980+nikki2906@users.noreply.github.com>
2026-03-19 02:28:34 -0400

    LC-minimum-absolute-difference [Runtime: 60 ms; Memory: 31.3 MB]

--- sorting/minimum-absolute-difference.py
+++ sorting/minimum-absolute-difference.py
@@ -6,7 +6,7 @@ class Solution:
         for i in range(1, len(arr)-1):
             diff = arr[i+1] - arr[i]
             if diff < minDiff:
-                minDiff == diff
+                minDiff = diff
         
         result = []
         for i in range(len(arr)-1): 
